f43c08691e5065255ca2b8dfdde867a3097d0602
Keeley Carrigan <keeley.carrigan@gmail.com>
2017-11-15 06:37:37 -0500

    Deleted Removed Modules From Export

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 import AutoIcon from './images/auto-icon';
 import AutoTouchable from './touch/auto-touchable';
-import BackgroundImg from './images/background-image';
-import BaseList from './lists/base-list';
 import ConditionalBlock from './utility/conditional-block';
 import KeyboardView from './utility/keyboard-view';
 import ListItem from './lists/list-item';
 import ModalCustom from './modals/modal-custom';
 import ProgressiveImage from './images/progressive-image';
-import StaticList from './lists/static-list';
 
 export {
 	AutoIcon,
 	AutoTouchable,
-	BackgroundImg,
-	BaseList,
 	ConditionalBlock,
 	KeyboardView,
 	ListItem,
 	ModalCustom,
-	ProgressiveImage,
-	StaticList
+	ProgressiveImage
 }
